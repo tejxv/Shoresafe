610c6b6a0649e9caf1fb8db4b57d95c33b00f88b
tejas <upadhyaytejas100@gmail.com>
2022-03-28 00:58:18 +0530

    init

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,22 +1,43 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlMrgvMnufzgSaKMrEHgMe7Qkc0XNihjo",
-  authDomain: "marine-21ee0.firebaseapp.com",
-  projectId: "marine-21ee0",
-  storageBucket: "marine-21ee0.appspot.com",
-  messagingSenderId: "650339080137",
-  appId: "1:650339080137:web:ce325f53aeed0600d83a18"
+  apiKey: "AIzaSyAzPfuvryU9N3WHQ-vB7a0aY1cHABXQsAU",
+  authDomain: "marine2-f2279.firebaseapp.com",
+  projectId: "marine2-f2279",
+  storageBucket: "marine2-f2279.appspot.com",
+  messagingSenderId: "288690639823",
+  appId: "1:288690639823:web:9a5dc6c72527f056a1f8ba",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// ORIGINAL CODE BELOW
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+// import { getFirestore } from 'firebase/firestore';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDlMrgvMnufzgSaKMrEHgMe7Qkc0XNihjo",
+//   authDomain: "marine-21ee0.firebaseapp.com",
+//   projectId: "marine-21ee0",
+//   storageBucket: "marine-21ee0.appspot.com",
+//   messagingSenderId: "650339080137",
+//   appId: "1:650339080137:web:ce325f53aeed0600d83a18"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import { auth, db } from "../firebase-config";
 
-function Home() {
+function Home({ isAuth }) {
   const [postLists, setPostList] = useState([]);
   const postsCollectionRef = collection(db, "posts");
 
@@ -10,15 +10,41 @@ function Home() {
     const getPosts = async () => {
       const data = await getDocs(postsCollectionRef);
       setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-      console.log(data);
     };
 
     getPosts();
-  });
+  }, []);
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+  };
   return (
     <div className="homePage">
       {postLists.map((post) => {
-        return <div className="posts"> {post.title} </div>;
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1> {post.title}</h1>
+              </div>
+              <div className="deletePost">
+                {isAuth && post.author.id === auth.currentUser.uid && (
+                  <button
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    {" "}
+                    &#128465;
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="postTextContainer"> {post.postText} </div>
+            <h3>@</h3>
+          </div>
+        );
       })}
     </div>
   );
